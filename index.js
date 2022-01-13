30ae9c6976559e62bdd368efccaea7435139a255
charles w carden <charles@charlescarden.com>
2022-01-13 08:35:35 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+
+//Setup Player
+
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.width = 100
+        this.height = 100
+    }
+    
+}
