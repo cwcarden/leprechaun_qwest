b5899bcfb704d7ee9870ebf14afe757ddf9e89a5
charles w carden <charles@charlescarden.com>
2022-01-13 08:51:55 -0600

    add css stylesheet and player constructors

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
-//Setup Player
+canvas.width = innerWidth;
+canvas.height =  innerHeight;
 
+
+//Setup Player
 class Player {
     constructor() {
         this.position = {
             x: 100,
             y: 100
         }
-        this.width = 100
-        this.height = 100
+        this.velocity = {
+            x: 0,
+            y: 1
+        }
+        this.width = 30;
+        this.height = 30;
     }
 
-    draw()
-    
+    draw() {
+        context.fillStyle = 'red'
+        context.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
 }
+
+const player = new Player()
+player.draw()
