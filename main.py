028fc518f471e8d350e17f72a2890814cac1cae0
charles <charles@charlescarden.com>
2022-02-28 12:40:56 -0600

    Add pipenv and pygame deps

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class Game:
                     sys.exit()
                     
             self.screen.fill('black')
-            pygame.displya.update()
+            pygame.display.update()
             self.clock.tick(FPS)
             
 
